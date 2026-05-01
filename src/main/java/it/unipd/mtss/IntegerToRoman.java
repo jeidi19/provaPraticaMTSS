7c329a77a486e38a41b00d3d22677f3401d959db
jd <japodedo@gmail.com>
2026-05-01 20:53:16 +0200

    close #1

---
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////
+// Antonio Tufano 2137972 
+// Jacopo De Donà 2104894 
+////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+public class IntegerToRoman {
+    public static String convert(int number) {
+        if (number < 1 || number > 1000) {
+            throw new IllegalArgumentException("Number out of range (1-1000)");
+        
+        StringBuilder roman = new StringBuilder();
+        while (number >= 1) {
+            roman.append("I");
+            number -= 1;
+        }
+        return roman.toString();
+    }
+}
---
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -0,0 +1,35 @@
+////////////////////////////////////////////////////////////////////
+// Antonio Tufano 2137972 
+// Jacopo De Donà 2104894 
+////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+public class RomanPrinter {
+    public static String print(int num) {
+        return printAsciiArt(IntegerToRoman.convert(num));
+    }
+
+    private static String printAsciiArt(String romanNumber) {
+        String[] letterI = {
+            "  _____  ",
+            " |_   _| ",
+            "   | |   ",
+            "   | |   ",
+            "  _| |_  ",
+            " |_____| "
+        };
+
+        StringBuilder result = new StringBuilder();
+        int height = 6;
+
+        for (int i = 0; i < height; i++) {
+            for (char c : romanNumber.toCharArray()) {
+                if (c == 'I') {
+                    result.append(letterI[i]);
+                }
+            }
+            result.append("\n");
+        }
+        return result.toString();
+    }
+}
